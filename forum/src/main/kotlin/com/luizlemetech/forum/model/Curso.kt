482c5f5a90b6d53b40890ad21fd86e99fa3831c1
Luiz Henrique Leme <luizleme.tech@gmail.com>
2025-04-08 21:16:45 -0300

    refactor: adicionadas as annotations para identificacao de entidade de banco, Id, estrategia de geracao e cardinalidade

--- forum/src/main/kotlin/com/luizlemetech/forum/model/Curso.kt
+++ forum/src/main/kotlin/com/luizlemetech/forum/model/Curso.kt
@@ -1,6 +1,13 @@
 package com.luizlemetech.forum.model
 
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity
 data class Curso (
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
     val nome: String,
     val categoria: String
