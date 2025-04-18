e52b632f8eedd73abae29d63e61fc986c22ccefe
Luiz Henrique Leme <luizleme.tech@gmail.com>
2025-04-17 22:40:49 -0300

    chore: criado script de inicializacao de dados no banco

---
+++ forum/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+-- Inserir dados na tabela curso
+INSERT INTO curso (nome, categoria) VALUES ('Kotlin', 'PROGRAMACAO');
+
+-- Inserir dados na tabela usuario
+INSERT INTO usuario (nome, email) VALUES ('Ana da Silva', 'ana@email.com');
