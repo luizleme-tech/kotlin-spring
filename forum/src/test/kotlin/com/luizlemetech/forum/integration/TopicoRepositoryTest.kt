ba97d5fe2e3df0b0b3650d076d27382e1a35dfda
Luiz Leme <luizleme.tech@gmail.com>
2025-10-21 22:07:37 -0300

    test: adiciona integração do TopicoRepository com Testcontainers
    - Criado arquivo de teste `TopicoRepositoryTest` com testes de integração.
    - Configurada instância do MySQL com o Testcontainers para testes.
    - Adicionadas dependências do Testcontainers ao `pom.xml`.

---
+++ forum/src/test/kotlin/com/luizlemetech/forum/integration/TopicoRepositoryTest.kt
@@ -0,0 +1,64 @@
+package com.luizlemetech.forum.integration
+
+import com.luizlemetech.forum.dto.TopicoPorCategoriaDto
+import com.luizlemetech.forum.model.TopicoTest
+import com.luizlemetech.forum.repository.TopicoRepository
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.data.domain.PageRequest
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.MySQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+
+@DataJpaTest
+@Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class TopicoRepositoryTest {
+
+    @Autowired
+    private lateinit var topicoRepository: TopicoRepository
+
+    private val topico = TopicoTest.build()
+
+    companion object {
+        @Container
+        private val mysqlContainer = MySQLContainer<Nothing>("mysql:8.0.28")
+            .apply {
+                withDatabaseName("testedb")
+                withUsername("testuser")
+                withPassword("123456")
+                withReuse(true)
+            }
+
+        @JvmStatic
+        @DynamicPropertySource
+        fun properties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url", mysqlContainer::getJdbcUrl)
+            registry.add("spring.datasource.password", mysqlContainer::getPassword)
+            registry.add("spring.datasource.username", mysqlContainer::getUsername)
+            registry.add("spring.jpa.hibernate.ddl-auto") { "validate" }
+        }
+    }
+
+    @Test
+    fun `deve gerar um relatorio`() {
+        topicoRepository.save(topico)
+        val relatorio = topicoRepository.relatorio()
+
+        assertThat(relatorio).isNotNull
+        assertThat(relatorio.first()).isExactlyInstanceOf(TopicoPorCategoriaDto::class.java)
+    }
+
+    @Test
+    fun `deve listar topico pelo nome do curso`() {
+        topicoRepository.save(topico)
+        val topico = topicoRepository.findByCursoNome(topico.curso.nome, PageRequest.of(0, 5))
+
+        assertThat(topico).isNotNull
+    }
+}
